dfdf6de1960f15a933afcc11dc2404535da48df3
Mikko Ykspetäjä <53930266+mikyks1@users.noreply.github.com>
2023-11-08 19:26:30 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/backend/utils/list_helper.js
+++ osa4/backend/utils/list_helper.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-unused-vars */
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce((likes, blog) => likes + blog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }

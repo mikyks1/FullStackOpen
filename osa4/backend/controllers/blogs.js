bff766bcf82cc68aa849960d7c460c7d19518e8b
Mikko Ykspetäjä <53930266+mikyks1@users.noreply.github.com>
2023-11-20 16:50:19 +0200

    4.17: blogilistan laajennus, step4

--- osa4/backend/controllers/blogs.js
+++ osa4/backend/controllers/blogs.js
@@ -1,15 +1,23 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate("user", { username: 1, name: 1, id: 1 })
   res.json(blogs)
 })
 
 blogsRouter.post("/", async (req, res) => {
+  const user = await User.findById("655777a1d928106a8adda0f3")
+
   const blog = new Blog(req.body)
+  blog.user = user._id
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog)
+  await user.save()
+
   res.status(201).json(savedBlog)
 })
 
